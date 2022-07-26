b585fa4b5dd2ee6e6ee608e3f340303acfb6676b
durveshramane26 <durveshramane6@gmail.com>
2022-07-26 12:54:15 +0530

    Bubble Sort

---
+++ Sorting/BubbleSort.py
@@ -0,0 +1,17 @@
+def BubbleSort(list1):
+    for i in range(len(list1)):
+        for j in range(len(list1)-i-1):
+            if list1[j] > list1[j+1]:
+                temp = list1[j]
+                list1[j] = list1[j+1]
+                list1[j+1] = temp
+                
+    print(list1)
+
+
+list1 = []
+for i in range(6):
+    b=int(input("Enter the number: "))
+    list1.append(b)
+
+BubbleSort(list1)
