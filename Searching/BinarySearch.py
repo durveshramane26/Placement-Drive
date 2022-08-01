c16e8e09b222fe6301ac3a35f270b4e72b689355
durveshramane26 <durveshramane6@gmail.com>
2022-08-01 11:22:27 +0530

    Binary Search

---
+++ Searching/BinarySearch.py
@@ -0,0 +1,30 @@
+from unittest import result
+
+
+def BinarySearch(list1, low, high, x):
+    if high >= low:
+        mid = (high + low) // 2
+
+        if list1[mid] == x:
+            return mid
+
+        elif list1[mid] > x:
+            return BinarySearch(list1, low, mid - 1, x)
+
+        else:
+            return BinarySearch(list1, mid + 1, high, x)
+
+    else:
+        return -1
+
+
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+x = 10
+
+result = BinarySearch(list1, 0, len(list1) - 1, x)
+
+if(result == -1):
+    print("Element not found")
+else:
+    print("Element found at index", result)
+    print("Element is", list1[result])
