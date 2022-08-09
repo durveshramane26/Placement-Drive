57540170947a772b675277dc49a5e3a982c0b516
durveshramane26 <durveshramane6@gmail.com>
2022-08-09 20:50:27 +0530

    write a function which takes parameter n and returns all the numbers 1 to n which are squares of another number

--- Problem-Solving/Question5.js
+++ Problem-Solving/Question5.js
@@ -1,16 +1,22 @@
 // write a function which takes parameter n and returns all the numbers 1 to n which are squares of another number
 
+// const squareRoot = (n) => {
+//     for (let i = 1; i <= n; i++) {
+//         for ( let j = 1 ; j <= n; j++) {
+//             if( j*j == i) {
+//                 console.log(i)
+//             }
+//         }
+//     }
+// }  
+
 const squareRoot = (n) => {
     for (let i = 1; i <= n; i++) {
-        for ( let j = 1 ; j <= n; j++) {
-            if( j*j == i) {
-                console.log(i)
-            }
+        if (i**0.5 == Math.floor(i**0.5)) {
+            console.log(i)
         }
     }
-}  
-
-
+}
 
 n=25;
 console.log(squareRoot(n))
