09821ffa2c1cb3fafab6ab805549e7b675ec926d
durveshramane26 <durveshramane6@gmail.com>
2022-09-19 12:01:13 +0530

    nums[0] + nums[1] == 9, we return [0, 1].

--- Problem-Solving/Question9.js
+++ Problem-Solving/Question9.js
@@ -6,15 +6,29 @@
 
 
 
+// const twoSum = (nums, target) => {
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] + nums[j] === target) {
+//                 return [i, j];
+//             }
+//         }
+//     }
+// }
+
 const twoSum = (nums, target) => {
+    let storage = {};
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
+        let current = nums[i];
+        let diff = target - current;
+        if (storage[diff] !== undefined) {
+            return [storage[diff], i];
         }
+        storage[current] = i;
     }
+
 }
+
 
 const nums = [2, 7, 11, 15];
 const target = 9;
